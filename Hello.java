54781c754dda7b9554e46bfb984409561633d578
aminesehnouni <89495214+aminesehnouni@users.noreply.github.com>
2021-08-24 21:30:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************
+* Hello Java
+************************************
+* Amine Sehnouni
+* 08/23/2021
+* CMSC 255-004
+***********************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Amine Sehnouni");
+		System.out.println("Hello Java");
+		System.out.println("Lab 1");
+		System.out.println("08/23/2021");
+		System.out.println("CMSC 255-004");
+		System.out.println();
+	}
+}
